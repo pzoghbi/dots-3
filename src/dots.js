f48bd5a3304b1d269ee8a952ad951f06ea04c5b9
Phillip Zoghbi <phillip.zoghby@gmail.com>
2022-09-20 00:56:45 +0200

    dots 3 effect

--- src/dots.js
+++ src/dots.js
@@ -24,56 +24,16 @@ window.addEventListener("resize", ()=>{
     canvas.height = Math.max(window.innerWidth, window.innerHeight);
 })
 
-class RenderingContext {
-    RenderingContext(canvas) {
-
-    }
-
-    draw(){
-
-    }
-
-    initGrid(){
-
-    }
-
-    clear() {
-        ctx.clearRect(0, 0, )
-    }
-}
-
-class Dot {
-    constructor(x, y) {
-        this.x = x
-        this.y = y;
-        this.defaultRadius = 10;
-        this.radius = this.defaultRadius;
-        this.color = {r: 0, g: 0, b: 0, a: 1};
-    }
-}
-
 let maxRowsCols = 29;
 let canvasRatio = Math.min(canvas.width / canvas.height, canvas.height / canvas.width)
-
-
-    
-
-let dots = [];
-for(let i = 0; i < maxRowsCols; i++){
-    for(let j = 0; j < maxRowsCols; j++){
-        // todo: (calculate maxrowscols)
-        dots.push(new Dot(i*50, j*50))
-    }
-}
-console.log(dots)
 
 function draw() {
     
     // RESET
     ctx.clearRect(0, 0, canvas.width, canvas.height);
     let gradient = ctx.createLinearGradient(0, 0, 0, window.innerHeight)
-    gradient.addColorStop(0, "black")
-    gradient.addColorStop(1, "black")
+    gradient.addColorStop(0, "crimson")
+    gradient.addColorStop(1, "royalblue")
     ctx.fillStyle = gradient;
     // ctx.fillStyle = "crimson";
     ctx.fillRect(0, 0, canvas.width, canvas.height);
@@ -125,13 +85,13 @@ function draw() {
             if (applyVfx) {
                 ctx.beginPath();
                 let hue = parseInt(distanceRatio * 360);
-                let sat = parseInt(distanceRatio * 50+50);
-                let val = parseInt(m.sin(distanceRatio) * 20 + 80);
+                let sat = parseInt(distanceRatio * 50+70);
+                let lit = parseInt(m.sin(distanceRatio) * 20 + 80);
                 // ctx.fillStyle = "hsl(" + hue + ", " + sat + "%, " + val + "%)"; // colorful
                 // let broj = clamp0((-1 / i) + (sinrad(Math.pow(i+1, 2))) - 1 - 
                 // (   Math.pow(cos(x) + cos(time/10), 2) - 1 + 
                 //     Math.pow(sin(y) + sin(time/10), 2) - 1), 1)
-                ctx.fillStyle = `hsl(${hue + 180*m.sinrad(time*x)}, ${sat}%, ${val}%)`;
+                ctx.fillStyle = `hsl(${hue + 180*m.sinrad(time/4*x)}, ${0}%, ${lit + m.sinrad(time/2*m.sin(x))}%, .1)`;
                 // ctx.fillStyle = "white"
                 // ctx.strokeStyle = "white"
                 // let _radius = 2.5 * radius / 3 + radius / 6 * (Math.sin(time * 2 * Math.PI / 360))
@@ -151,12 +111,14 @@ function draw() {
                 
             } else {
                 ctx.beginPath();
-                ctx.fillStyle = "white"
+                ctx.fillStyle = "rgba(255, 255, 255, "+ (.1)+")"
 
                 let size = radius;
+                let calculatedX = x + m.cosrad(x) * 3 - 3 + m.cosrad(y) - 1;
+                let calculatedY = y + m.sinrad(y) * 3 - 3 + m.sinrad(y) - 1;
                 ctx.ellipse(
-                    x + m.cosrad(x) * 3 - 3 + 1 * m.cosrad(y) - 1,
-                    y + m.sinrad(y) * 3 - 3 + 1 * m.sinrad(y) - 1,
+                    calculatedX,
+                    calculatedY,
                     size, size,
                     0, 0, Math.PI * 2);
             }
@@ -164,15 +126,6 @@ function draw() {
             ctx.fill();
         }
     }
-
-
-    ctx.fillStyle = ("rgb(0, 162, 255)")
-    ctx.fillRect(250, 180, 540, 180)
-    ctx.strokeStyle = ("white")
-    ctx.lineWidth = 3
-    ctx.strokeRect(260, 190, 520, 160)
-    ctx.fillStyle = "white"
-    ctx.fillText("Hello, tasks for today:", 290, 235)
 
     time += .01;
     yoff += .1;
